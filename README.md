0825658e545bf68764b7e27f736d49ab4f927f96
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-05 22:16:58 +0000

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Control Flow
+Repo created to test the use of control flow in javascript
+
+### Task
+> You don't know if you can afford to buy another drink. You decide that you ***will***, if you have more than £10. If not you should go .
+
+>If i have more than £10 in my pocket AND I've had fewer than three drinks I'll buy another drink. If I have more than £5 OR the crisps are free I'll buy some crisps. Otherwise I'll go home.
---
+++ main.js
@@ -0,0 +1,39 @@
+//
+// let moneyInPocket = prompt('how much do you have to spend?');
+//
+// if (moneyInPocket > 10) {
+//   console.log('I can buy a drink with my lunch');
+// } else if (moneyInPocket >5) {
+//   console.log("i can buy some crisps");
+// } else {
+//   console.log("I can't buy anything" );
+// }
+
+// var moneyInPocket = prompt('how much money in pocket?');
+// var drinksHad = prompt('How many drinks had ?');
+// var crispsPrice = prompt('Crisps price?');
+//
+// if (moneyInPocket > 10 && drinksHad < 3) {
+//   console.log('buy another drink');
+// } else if (moneyInPocket > 5 || crispsPrice < 1) {
+//   console.log('buy some crisps');
+// } else {
+//   console.log('go home');
+// }
+var name = 'tim'
+if (name === 'bob') {
+  console.log('Hi bob');
+} else if (name === 'tim') {
+  console.log('hi Tim');
+}
+switch (name) {
+  case "bob":
+    console.log('Hi bob');
+    break;
+    case 'tim':
+    console.log('hi tim');
+
+      break;
+  default:
+
+}
